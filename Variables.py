4f4c224c8bcb7462e36837da6603f7f8255cac0e
ZaRyAaB0419 <kinghereah@gmail.com>
2024-09-03 20:53:37 +0500

    Add files via upload

---
+++ Variables.py
@@ -0,0 +1,9 @@
+x=192403
+y="REkhA"
+
+print(x)
+print(y)
+
+name=input("Enter Your Name:")
+
+print("\n Hello", "\n Welcome To CODINGAL")
---
+++ Welcome.py
@@ -0,0 +1,14 @@
+ #print a sentence
+print("Welcome to the World of Fear")
+
+ #print a number
+print(192403)
+
+ #next line after print
+print("Hello my friend Python \n")
+
+ #pass multiple arguments
+print("Rekha", 192403)
+
+ #end argument
+print("I love Codingal", end= "*")
